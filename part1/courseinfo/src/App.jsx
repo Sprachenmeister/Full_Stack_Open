6050c415391405b627ba2a73af572f1ed92b4bfb
Michelle Plascencia <michellefutbool@gmail.com>
2026-05-05 18:33:54 -0600

    bug fix

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -54,9 +54,9 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content 
-        part1={part1} exercises1={part1.exercises}
-        part2={part2} exercises2={part2.exercises}
-        part3={part3} exercises3={part3.exercises}
+        part1={part1.name} exercises1={part1.exercises}
+        part2={part2.name} exercises2={part2.exercises}
+        part3={part3.name} exercises3={part3.exercises}
       />
       <Total 
         exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} 
