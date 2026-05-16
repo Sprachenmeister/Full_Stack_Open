e9239c257a50a35c19903245cacab3f7799cfff2
Michelle Alejandro Plascencia Martinez <michellefutbool@gmail.com>
2026-05-16 16:49:20 -0600

    unicafe exercise update

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,23 +8,27 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
+  const [allClicks, setAll] = useState(0)
 
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
     setTotal(updatedGood + neutral + bad)
+    setAll(allClicks + 1)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
     setTotal(good + updatedNeutral + bad)
+    setAll(allClicks + 0)
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
     setTotal(good + neutral + updatedBad)
+    setAll(allClicks - 1)
   }
 
   return (
@@ -35,7 +39,7 @@ const App = () => {
       <Button onClick={handleBadClick} text="bad"/>
       <h1>statistics</h1>
       <p>good {good} <br />neutral {neutral} <br /> bad {bad} 
-      <br /> all {total} <br /> average <br/> positive </p>
+      <br /> all {total} <br /> average {allClicks} <br/> positive </p>
     </div>
   )
 }
