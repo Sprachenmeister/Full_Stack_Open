4b183a0c95e7c9c33ba3e9f38bf1be35c19ea0e3
Michelle Alejandro Plascencia Martinez <michellefutbool@gmail.com>
2026-05-17 15:39:41 -0600

    next anecdote button added

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, text }) => (<button onClick={onClick}>{text}</button>) 
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -14,9 +16,17 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
 
+  const anecdoteClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+
+
   return (
     <div>
-      {anecdotes[selected]}
+        <p>{anecdotes[selected]}</p>
+      <Button onClick={anecdoteClick} text="next anecdote" />
     </div>
   )
 }
