716c358f1a25157ef4cf56765b8387d863336a1d
Michelle Alejandro Plascencia Martinez <michellefutbool@gmail.com>
2026-05-30 22:35:13 -0600

    App.jsx update

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,24 +1,49 @@
 import { useState } from 'react'
 
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject ={
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNewName =(event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName}
+          onChange={handleNewName}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+         {persons.map((person, index) => (
+          <li key={index}>{person.name} {person.content}</li>
+        ))}
+      </ul>
     </div>
   )
 }
