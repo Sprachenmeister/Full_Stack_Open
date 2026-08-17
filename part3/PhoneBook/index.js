c1c7bfec49ef36b7a96e6ce4e1906fd9c5462c31
Michelle <michellefutbool@gmail.com>
2026-08-17 11:26:06 -0600

    update

--- part3/PhoneBook/index.js
+++ part3/PhoneBook/index.js
@@ -108,5 +108,5 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, ()=>
+    console.log(`Server running on port ${PORT}`))
